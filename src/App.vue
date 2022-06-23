e0d6cef8f7cdb72b972ee187cdb3e0a1a15bfbf0
Gabriel Passos <kooujinproductions@gmail.com>
2022-06-23 12:19:53 -0300

    Added Card session with static data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script>
   import Header from './components/Header.vue'
+  import Card from './components/Card.vue'
   
   export default {
     name: 'App',
     components: {
       Header,
+      Card
     },
   }
 </script>
 
 <template>
   <Header />
+  <Card />
 </template>
 
 <style>
---
+++ src/components/Card.vue
@@ -0,0 +1,128 @@
+<script>
+  export default {
+    name: 'Card',
+    props: {
+      heroName: String,
+      heroDescription: String,
+      hero: Object
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <!-- <div class="star"/> -->
+    <img src="../assets/iron-man.png" alt="">
+    <div>
+      <h1>Iron Man</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, maxime. Esse reiciendis
+        pariatur quas, aperiam cupiditate tenetur commodi unde, delectus ipsum consequatur autem soluta
+        neque possimus sunt dignissimos assumenda minima.
+      </p>
+    </div>
+
+  </main>
+</template>
+
+<style scoped>
+  .star {
+  --star-color:orange;
+  margin: 1em auto;
+  font-size: 1em;
+  position: relative;
+  display: block;
+  width: 0px;
+  height: 0px;
+  border-right: 1em solid transparent;
+  border-bottom: 0.7em solid var(--star-color);
+  border-left: 1em solid transparent;
+  transform: rotate(35deg);
+  }
+  .star:before {
+  border-bottom: 0.8em solid var(--star-color);
+  border-left: 0.3em solid transparent;
+  border-right: 0.3em solid transparent;
+  position: absolute;
+  height: 0;
+  width: 0;
+  top: -0.45em;
+  left: -0.65em;
+  display: block;
+  content:"";
+  transform: rotate(-35deg);
+  }
+  .star:after {
+  position: absolute;
+  display: block;
+  top: 0.03em;
+  left: -1.05em;
+  width: 0;
+  height: 0;
+  border-right: 1em solid transparent;
+  border-bottom: 0.7em solid var(--star-color);
+  border-left: 1em solid transparent;
+  transform: rotate(-70deg);
+  content:"";
+  }
+
+  main {
+    width: 100vw;
+    height: calc(100vh - 80px);
+    background-color: rgb(58, 58, 58);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    scroll-snap-align: start;
+  }
+
+  main
+  img {
+    margin-top: 5.5rem;
+    height: 30rem;
+  }
+  div {
+    max-width: 25.125rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+    font-family: 'Bebas Neue', sans-serif;
+    font-size: 4rem;
+    color: var(--title);
+
+    margin-top: 2.5rem;
+    margin-bottom: 2.5rem;
+    text-shadow: 0 2px 0 black;
+  }
+  p {
+    font-family: 'Inter', sans-serif;
+    font-size: 1.1rem;
+    color: var(--description);
+    text-align: center;
+    text-shadow: 0 1px 0 black;
+  }
+  @media (max-width: 1080px) {
+    main {
+      height: calc(100vh - 70px);
+    }
+    img {
+      max-height: 370px;
+    }
+  }
+  @media (min-width: 1080px) {
+    main {
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      max-width: calc(100vw - 18px);
+    }
+    img {
+      margin-right: 8rem;
+    }
+  }
+</style>
